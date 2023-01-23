0973be25446b5f2be8f1d1c9954eaa8223f5eb38
TinTinWinata <tintin6892@gmail.com>
2023-01-23 20:11:42 +0700

    make ui better

--- lawracasts-app/resources/views/pages/podcast/podcast.blade.php
+++ lawracasts-app/resources/views/pages/podcast/podcast.blade.php
@@ -7,6 +7,6 @@
     <div class="max-w-max flex gap-2 mx-auto" style="margin-left: 31%">
         <x-podcast.podcasts-container :podcasts="$podcasts" :selected="$selected" />
     </div>
-    <x-podcast.edit-podcast-form :edit="$edit" />
+        <x-podcast.edit-podcast-form :edit="$edit" />
 
 @endsection
