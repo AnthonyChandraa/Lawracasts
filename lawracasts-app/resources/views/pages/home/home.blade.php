075b47c2aab4ccf715943bb8d8df752539d2310f
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2023-01-25 09:13:43 +0700

    change route login to index
    - Salah route login pas tembak url profil saat blm login

--- lawracasts-app/resources/views/pages/home/home.blade.php
+++ lawracasts-app/resources/views/pages/home/home.blade.php
@@ -5,6 +5,5 @@
 @section('content')
     <div class="max-w-7xl mx-auto flex justify-center gap-2 px-4 sm:px-6 lg:px-8">
         <img class="w-1/2" src="{{ asset('storage/assets/home-illustration.svg') }}" alt="" srcset="">
-
     </div>
 @endsection
